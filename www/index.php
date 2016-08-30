c411c274cb1c69788b05029060af462915400584
Dimitrii222 <ghetmanenco.dimitrii@mail.ru>
2016-08-30 19:04:48 +0300

    Add files via upload

---
+++ www/index.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title>WAPIK</title>
+	<link href="/css/style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<header>
+	<div id="logo">
+	<a href="/" title="WAPIK" ><span>WAPIK</span></a>  |  <a href="" title="phone">+62 202 555 0117</a>
+	</div>
+	<div id="headMenu">
+	<a href="/" title="home">HOME</a>
+	<a href="about.php" title="about us">ABOUT US</a>
+	<a href="pricing.php" title="pricing">PRICING</a>
+	<a href="contactUs.php" title="contact us">CONTACT US</a>
+	<a href="registration.php" title="registration">GET STARTED</a>
+	</div>
+</header>
+
+<div id="wrapper">
+	<div id="featBlock">
+		<div id="features">
+		<h2>Best of our features</h2>
+		<center><hr /></center>
+		<p>Lorem Ipsum is simply dummy text of the printing</p>
+		<p>Lorem Ipsum has been the industry's standard dummy text</p>
+		</div>
+		<div id="ionicons">
+		<img src="/img/icons/hydrogen.png"/>
+		<h4>Ionicons</h4>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+		</div>
+		<div id="design">
+		<img src="/img/icons/kolba.png"/>
+		<h4>Creative Disign</h4>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+		</div>
+		<div id="photo">
+		<img src="/img/icons/ph.png"/>
+		<h4>Photoshop and Sketch</h4>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+		</div>
+		<div class="clear"><br /><br /></div>
+		<div id="cross">
+		<h4><img src="/img/icons/globe.png"/> Cross Browser Compatibility</h4>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+		</div>
+		<div id="idea">
+		<h4><img src="/img/icons/lamp.png"/> Creative Idea</h4>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+		</div>
+		<div  id="pixel">
+		<h4><img src="/img/icons/wand.png"/> Pixel Perfect</h4>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+		</div>
+		<div id="free">
+		<h4><img src="/img/icons/cloud.png"/> Free Forever and Ever</h4>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+		</div>
+	</div>
+	
+	<div id="phoneBlock">
+		<h2>Lookinf for the perfect template to use?</h2>
+		<center><hr /></center>
+		<p>Lorem Ipsum is simply dummy text of the printing</p>
+		<div id="imgblock">
+		<img src="/img/handphone.png" />
+		</div>
+		<div id="text">
+		<h2>Responsive Ready</h2><br />
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+		<ul>
+			<li>Lorem Ipsum is simply dummy text</li>
+			<li>printing and typesetting industry</li>
+		</ul>
+		</div>
+	</div>
+	
+	<div id="quotation">
+		<div id="qoute">
+			<blockquote>
+					<p>This theme is beautiful, elegant and easy to customize. I've been able to build an amazing site in just a minute. Thank you very much fo creating this impressive template!</p><br />
+				<span id="name">Valeria Boltneva</span><br />
+				<span id="status">Director of Acme.Inc</span>
+			</blockquote>
+		</div>
+		<div id="photo">
+			<img src="/img/VB.jpeg" />
+		</div>
+	</div>
+	
+	<div id="details">
+		<div id="detTeaxt">
+			<h2>Built with High Attention to Details</h2>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+		</div>
+		<div id="dotmap">
+			<img src="/img/world-map.png" />
+		</div>
+	</div>
+	
+	<div id="pricing">
+		<div id="PricingAndPlan">
+			<h2>Pricing & Plan</h2>
+			<center><hr /></center>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+		</div>
+		<div id="treeBlocks">
+			<div id="basic">
+				<h3>BASIC</h3><br />
+				<h1>$199</h1>
+				<p>/mount</p><br />
+				<p>Justo Fringilla</p><br />
+				<p>Molis Sir Nullam</p><br />
+				<p>Ulricies Purus Amet</p><br />
+				<p>Crass Inceptos</p><br />
+				<input type="button" value="SELECT PLAN" title="select plan" id="select_plan" />
+			</div>
+			<div id="plus">
+			<h3>PLUS</h3><br />
+				<h1>$399</h1>
+				<p>/mount</p><br />
+				<p>Justo Fringilla</p><br />
+				<p>Molis Sir Nullam</p><br />
+				<p>Ulricies Purus Amet</p><br />
+				<p>Crass Inceptos</p><br />
+				<input type="button" value="SELECT PLAN" title="select plan" id="select_plan" />
+			</div>
+			<div id="premium">
+			<h3>PREMIUM</h3><br />
+				<h1>$699</h1>
+				<p>/mount</p><br />
+				<p>Justo Fringilla</p><br />
+				<p>Molis Sir Nullam</p><br />
+				<p>Ulricies Purus Amet</p><br />
+				<p>Crass Inceptos</p><br />
+				<input type="button" value="SELECT PLAN" title="select plan" id="select_plan" />
+			</div>
+		</div>
+		<div id="allplans">
+			<h2>Included with all Plans</h2>
+			<center><hr /></center>
+			<ul>
+				<li>Lorem Ipsum is simply</li>
+				<li>dummy text of the printing</li>
+				<li>typesetting industry</li>
+			</ul>
+			<ul>
+				<li>Lorem Ipsum has been</li>
+				<li>the industry's standard</li>
+				<li>dummy text </li>
+			</ul>
+			<ul>
+				<li>an unknown printer</li>
+				<li>took a galley of type</li>
+				<li>make a type specimen book</li>
+			</ul>
+		</div>
+		<div class="clear"></div><br />
+		<div id="help">
+			<img src="/img/img.png"/>
+			<h2>Need Help?</h2>
+			<center><hr /></center>
+			<p>Contact our Customer Suuport that is always ready to help you with any possible questions, promlems or information.</p><br />
+			<a href="" target="blank">support@wappik.com</a>
+		</div>
+	</div>
+	
+	<div id="download">
+		<div id="dowtext">
+		<h1>Do You Like Wapik? Download it Now</h1>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+		</div>
+		<div id="dowbut">
+		<input type="button" value="DONWLOAD NOW!-FOREVER FREE"/>
+		</div>
+		<div class="clear"></div>
+	</div>
+	
+
+
+</div>
+
+<footer>
+	<div id="firstFootBlock">
+		<div id="wapik">
+			<h3>WAPIK</h3>
+			<p>@: <a href="" target="blank">hi@wapik.com</a></p><br />
+			<p>p: +62 202 555 0117</p><br />
+			<p>a: 610 overlook Circle Suite 323 Kalamazoo, MI 49009</p><br />
+		</div>
+		<div id="company">
+			<h4>Company</h4><br />
+			<a href="/" title="home">Home</a><br />
+			<a href="about.php" title="about us">About Us</a><br />
+			<a href="pricing.php" title="pricing">Pricing</a><br />
+			<a href="contactUs.php" title="contact us">Contact Us</a><br />
+		</div>
+		<div id="others">
+			<h4>Others</h4><br />
+			<a href="" title="">Help & Support</a><br />
+			<a href="" title="">Privacy Policy</a><br />
+			<a href="" title="">Terms</a><br />
+			<a href="" title="">Site Map</a><br />
+		</div>
+		<div id="newletter">
+			<h4>Newsletter</h4><br />
+			<p>Subscribe to our newletter and get all coll news</p><br />
+			<input type="text" placeholder="Enter Email" name="email" id="email"/><br />
+		</div>
+	</div>
+	<div id="secondFootBlock">
+		<div id="rights">
+		<p>All rights reserved <?=date('Y')?> &copy; </p> 
+		</div>
+		<div id="socialNets">
+			<a href="https://twitter.com" title="twitter" target="blank"><img src="/img/icons/tw.png" title="twitter" alt="twitter" /></a>
+			<a href="https://www.facebook.com" title="facebook" target="blank"><img src="/img/icons/fb.png" title="facebook" alt="facebook" /></a>
+			<a href="https://www.instagram.com" title="instagram" target="blank"><img src="/img/icons/inst.png" title="instagram" alt="instagram" /></a>
+			<a href="https://dribbble.com" title="dribbble" target="blank"><img src="/img/icons/dr.png" title="dribbble" alt="dribbble" /></a>
+			<a href="http://blog.catmoji.com/" title="catmoji" target="blank"><img src="/img/icons/cat.png" title="catmoji" alt="catmoji" /></a>
+		</div>
+	</div>
+</footer>
+</body>
+</html>
